7ca8c34f22e56cc79b99a8920b83f8643ec50a54
Patrick <labreche.bass@gmail.com>
2017-03-03 17:05:56 +0800

    Made a back-up of 'show.html.erb' called 'BUshow.html.erb' New version of 'show.html.erb' now used for testing Helper code, please ignore innapropriacy Helper code fully functional. Described below :
    Reads file, finds all sections called 'Part', removes them from string and into sub-strings that are then inserted in a String Array.
    Reads every 'Part' sub-string, finds all sections called 'Staff', removes them from sub-string and into sub-strings that are then inserted in a String Array.
    Reads every 'Staff' sub-string, finds the 'Staff ID', and enters it in a hash with its 'Staff' and 'Part' '# part => {staff => staff ID}'.
    Puts the 'Staff' sub-strings back into the 'Part' sub-strings.

--- app/helpers/ms_converters_helper.rb
+++ app/helpers/ms_converters_helper.rb
@@ -2,26 +2,73 @@ module MsConvertersHelper
   # ignore this
   # @ms_converter = MsConverter.find(params[:id])
 
-  part_exists = true
-  part_amount = 0
+  # part and staff variables used in code block that finds and isolates parts
   part_array = []
+  staff_array = []
+  # variables used for loops.
+  i = 0
+  j = 0
+  k = 0
+
+  sheet_info = Hash.new{|hsh,key| hsh[key] = {}}
 
   file = # @ms_converter.attachment.read
 
-    while part_exists
+    # Break sheet into parts
+    part_exists = true
+    while part_exists do
 
-    if (!(file =~ /Part/) == nil) && (!(file =~ /<\/Part>/) == nil)
-      part_index = file =~ /Part/
+    if ((file =~ /<Part>/) != nil) && ((file =~ /<\/Part>/) != nil)
+      part_index = file =~ /<Part>/
       end_part_index = (file =~ /<\/Part>/) + 7
 
       sub_file = file.slice!(part_index...end_part_index)
-      part_amount += 1
-      part_array.push(subFile)
+      part_array.push(sub_file)
 
     elsif ((file =~ /Part/) == nil) || ((file =~ /<\/Part>/) == nil)
       part_exists = false
 
     end
+    end
+  # Break parts into staves
+  while i < part_array.length do
+    staff_exists = true
+    while staff_exists do
 
+      if ((part_array[i] =~ /<Staff id=/) != nil) && ((part_array[i] =~ /<\/Staff>/) != nil)
+
+        staff_index = part_array[i] =~ /<Staff id=/
+        end_staff_index = (part_array[i] =~ /<\/Staff>/) + 8
+
+        sub_file = part_array[i].slice!(staff_index...end_staff_index)
+        staff_array.push(sub_file)
+
+      elsif ((part_array[i] =~ /<Staff id=/) == nil) || ((part_array[i] =~ /<\/Staff>/) == nil)
+
+        staff_exists = false
+      end
+     end
+     # Gather information from staves
+     j = 0
+     while j < staff_array.length do
+
+       staff_string = staff_array[j]
+       # sheet_info # part => {staff => staffID}
+       sheet_info[i].store(j, staff_string[11])
+       j += 1
+     end
+     # Put staves back into part before moving on to next part
+     k = 0
+     while k < staff_array.length do
+
+       k += 1
+       if k > 1
+
+         staff_array[(staff_array.length - k)].concat(" ")
+       end
+       part_array[i].insert(7, staff_array[(staff_array.length - k)])
+     end
+     staff_array.clear
+     i += 1
   end
 end
